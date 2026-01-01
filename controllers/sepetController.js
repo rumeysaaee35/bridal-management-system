90aea10fbaf44b5a22a80e64296b8611dee77cb7
rumeysaaerz <erizrumeyssa@gmail.com>
2026-01-01 12:52:25 +0300

    Update sepetController.js

--- controllers/sepetController.js
+++ controllers/sepetController.js
@@ -1,26 +1,21 @@
 import pool from "../config/db.js";
 
-// SEPETİ ONAYLA VE VERİTABANINA KAYDET
 export async function sepetiOnayla(req, res) {
     const connection = await pool.getConnection();
     try {
         await connection.beginTransaction();
 
         const { musteri_ad, musteri_soyad, telefon_no, adres, urunler } = req.body;
-        // urunler: [{ id: 1, adet: 1, fiyat: 15000, beden: '36' }, ...] şeklinde gelecek
-
+       
         for (const urun of urunler) {
             const toplam_tutar = Number(urun.fiyat) * Number(urun.adet);
 
-            // 1. Satış Tablosuna Ekle (magaza_satislar)
             await connection.query(`
                 INSERT INTO magaza_satislar 
                 (musteri_ad, musteri_soyad, telefon_no, adres, model_id, adet, birim_fiyat, toplam_tutar, islem_turu, beden, satis_tarihi)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'satis', ?, NOW())
             `, [musteri_ad, musteri_soyad, telefon_no, adres, urun.id, urun.adet, urun.fiyat, toplam_tutar, urun.beden]);
 
-            // 2. Stoktan Düş (adet tablosundan)
-            // Önce stok var mı kontrol et
             const [stokKontrol] = await connection.query("SELECT * FROM adet WHERE model_id = ?", [urun.id]);
             
             if(stokKontrol.length > 0) {
